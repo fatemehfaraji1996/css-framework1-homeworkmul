dbe3478e4ca38ba6ef24704ba007be01d005b228
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-07-30 20:19:44 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import AligendButtons from "./components/1-AlignedButtons/AlignedButtons";
+
+export default function App() {
+  return (
+    <div>
+    <AligendButtons/>    
+    </div>
+  );
+}
---
+++ src/components/1-AlignedButtons/AlignedButtons.jsx
@@ -0,0 +1,14 @@
+import { Button, TextField, Typography } from "@mui/material";
+
+export default function AligendButtons(){
+return(
+    <>
+    <Typography variant="h3" component="h1">
+  create your account
+</Typography>
+<TextField id="outlined-basic" label="firstName" variant="outlined" />
+<TextField id="outlined-basic" label="lastName" variant="outlined" />
+<TextField id="outlined-basic" label="age" variant="outlined" />
+    </>
+)
+}
