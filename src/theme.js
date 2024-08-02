12d57e3f2cb432ef13fcae4c1a14a7b4349a208e
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-03 01:17:06 +0330

    done

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,8 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
+
+        
       light: '#757ce8',
       main: '#3f50b5',
       dark: '#002884',
